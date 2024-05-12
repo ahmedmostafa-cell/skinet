ef02eb075a4cdb1dace183856ec1e20df602dedf
ahmedmostafa-cell <67522336+ahmedmostafa-cell@users.noreply.github.com>
2024-05-12 22:35:59 +0300

    Shift Work To Laptop

--- Core/Specifications/ProductsWothTypesAndBrandsSpecification.cs
+++ Core/Specifications/ProductsWothTypesAndBrandsSpecification.cs
@@ -9,10 +9,31 @@ namespace Core.Specifications
 {
     public class ProductsWothTypesAndBrandsSpecification : BaseSpecification<Product>
     {
-        public ProductsWothTypesAndBrandsSpecification()
+        public ProductsWothTypesAndBrandsSpecification(string sort, int? brandId, int? typeId)
+         : base(x =>
+
+            (!brandId.HasValue || x.ProductBrandId == brandId) &&
+            (!typeId.HasValue || x.ProductTypeId == typeId)
+         )
         {
             AddInclude(x => x.ProductType);
             AddInclude(x => x.ProductBrand);
+            AddOrderBy(x => x.Name);
+            if (!string.IsNullOrEmpty(sort))
+            {
+                switch (sort)
+                {
+                    case "priceAsc":
+                        AddOrderBy(P => P.Price);
+                        break;
+                    case "priceDesc":
+                        AddOrderByDescending(p => p.Price);
+                        break;
+                    default:
+                        AddOrderBy(x => x.Name);
+                        break;
+                }
+            }
         }
 
         public ProductsWothTypesAndBrandsSpecification(int id) : base(x => x.Id == id)
